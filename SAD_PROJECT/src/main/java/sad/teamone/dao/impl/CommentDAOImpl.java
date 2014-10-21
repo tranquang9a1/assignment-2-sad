a92a377aba0995e792ab66f8e68ef857f07c3649
nguyennkse61025 <nguyennkse61025@5554fd75-83a4-8eb5-928a-891a921fe516>
2014-10-21 18:13:46 +0000

    remove method findByCategory

--- SAD_PROJECT/src/main/java/sad/teamone/dao/impl/CommentDAOImpl.java
+++ SAD_PROJECT/src/main/java/sad/teamone/dao/impl/CommentDAOImpl.java
@@ -12,12 +12,11 @@ import java.util.List;
  * Created by QuangTV on 10/19/2014.
  */
 public class CommentDAOImpl extends BaseDAOImpl<Comment> implements CommentDAO<Comment> {
-    private static final String COMMENT = "Comment";
     private EntityManager em = emf.createEntityManager();
 
     @Override
     public List findByJobId(int jobID) {
-        Query query = em.createQuery("Select e From " + COMMENT + " e where e.jobID =:jobID");
+        Query query = em.createQuery("Select e From Comment e where e.jobID =:jobID");
         query.setParameter("jobID", jobID);
         return query.getResultList();
     }
