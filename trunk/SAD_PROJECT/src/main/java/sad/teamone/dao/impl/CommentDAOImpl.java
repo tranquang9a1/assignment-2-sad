5465238737ec42d252712be3be11782d86310d9a
nguyennkse61025 <nguyennkse61025@5554fd75-83a4-8eb5-928a-891a921fe516>
2014-10-21 02:09:08 +0000

    add method find comment by JobID

--- trunk/SAD_PROJECT/src/main/java/sad/teamone/dao/impl/CommentDAOImpl.java
+++ trunk/SAD_PROJECT/src/main/java/sad/teamone/dao/impl/CommentDAOImpl.java
@@ -3,9 +3,22 @@ package sad.teamone.dao.impl;
 import sad.teamone.dao.CommentDAO;
 import sad.teamone.entity.Comment;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
 
 /**
  * Created by QuangTV on 10/19/2014.
  */
 public class CommentDAOImpl extends BaseDAOImpl<Comment> implements CommentDAO<Comment> {
+    private static final String COMMENT = "Comment";
+    private EntityManager em = emf.createEntityManager();
+
+    @Override
+    public List findByJobId(int jobID) {
+        Query query = em.createQuery("Select e From " + COMMENT + " e where e.jobID =:jobID");
+        query.setParameter("jobID", jobID);
+        return query.getResultList();
+    }
 }
