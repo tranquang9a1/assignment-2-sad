69f1aa24e3e2a3534c91ccb8ba3208e68c06447f
vinaheo1407 <vinaheo1407@gmail.com>
2014-10-22 08:21:50 +0000

    add AdminController.java

---
+++ SAD_PROJECT/src/main/java/sad/teamone/controller/AdminController.java
@@ -0,0 +1,17 @@
+package sad.teamone.controller;
+
+import sad.teamone.common.annotation.Autowired;
+import sad.teamone.common.annotation.RequestMapping;
+import sad.teamone.service.UserService;
+
+import javax.annotation.RegEx;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by QuanVMH on 10/22/2014.
+ */
+public class AdminController {
+    
+
+}
